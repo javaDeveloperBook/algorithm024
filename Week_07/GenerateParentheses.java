0f0c3a2c2d3b1b46aca2957b88bac5baae54a060
wujie@cmiot.cmcc <wujie123456>
2021-03-17 17:47:18 +0800

    https://leetcode-cn.com/problems/generate-parentheses/

---
+++ Week_07/GenerateParentheses.java
@@ -0,0 +1,26 @@
+/**
+ * 22. 括号生成
+ * https://leetcode-cn.com/problems/generate-parentheses/
+ *
+ * 方式一：深度优先遍历
+ *
+ *
+ */
+class Solution {
+    public List<String> generateParenthesis(int n) {
+        List<String> result = new ArrayList<>();
+        if (n <= 0) return result;
+        dfs(result,"",n, 0, 0);
+        return result;
+    }
+
+    private void dfs(List<String> result,String temp, int n, int left, int right) {
+        if (left > n || right > n || right > left) return;
+        if (temp.length() == 2 * n) {
+            result.add(temp);
+            return;
+        }
+        dfs(result,temp + "(", n, left + 1,right);
+        dfs(result,temp + ")", n,left, right + 1);
+    }
+}
